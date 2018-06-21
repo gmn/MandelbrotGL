cf8c10ed2408be990bfcfc301ba505de89ce8547
Greg Naughton <greg@naughton.org>
2018-06-21 11:54:02 -0600

    Increasing the range of the HUE slider. Values of 800-1000 look really good.

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -1818,8 +1818,8 @@ public:
                 mandelbrot.GetSaturation( &saturation );
                 mandelbrot.GetVibrance( &vibrance );
 
-                double hlow = 10.0, hhigh = 500.0;
-                double slow = 0.1 , shigh = 5.0;
+                double hlow = 10.0, hhigh = 1000.0;
+                double slow = 0.1 , shigh = 2.0;
                 double vlow = 1.0 , vhigh = 50.0;
 
                 ImGui::SliderScalar( "Hue", ImGuiDataType_Double, hue, &hlow, &hhigh, "%.3lf", 1.0f );
