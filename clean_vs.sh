29311ca346c3d1591ec297e60eecc4cf93ab65c8
Der KapitanLeuitnant <Greg@Windows7>
2018-07-02 19:44:31 -0600

    fixed visual studio directories

--- clean_vs.sh
+++ clean_vs.sh
@@ -1,5 +1,7 @@
 rm -rf Debug Release
-rm -rf .vs glew SDL2
-rm -rf sdl_mandelbrot.vcxproj~
-rm -rf sdl_mandelbrot.vcxproj.user
-rm -rf .sdl_mandelbrot.vcxproj.un~
+rm -rf sdl2_opengl22/Debug sdl2_opengl22/Release
+rm -rf sdl2_opengl22/.vs
+rm -rf glew SDL2
+rm -rf sdl2_opengl22/sdl_mandelbrot.vcxproj~
+rm -rf sdl2_opengl22/sdl_mandelbrot.vcxproj.user
+rm -rf sdl2_opengl22/.sdl_mandelbrot.vcxproj.un~
